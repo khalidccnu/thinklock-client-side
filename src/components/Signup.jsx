5a14da79cd9986dc8e793d32782a631907ea4b1a
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-06-08 15:52:31 +0600

    regex validation added in signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IKContext, IKUpload } from "imagekitio-react";
 import toast from "react-hot-toast";
-import { FaUpload } from "react-icons/fa";
+import { FaInfoCircle, FaUpload } from "react-icons/fa";
 import useAuth from "../hooks/useAuth.js";
 
 const Signup = () => {
@@ -17,6 +17,8 @@ const Signup = () => {
     gender: "",
     role: "not set yet",
   });
+  const [passVal, setPassVal] = useState(false);
+  const [passValWarn, setPassValWarn] = useState({});
   const [firstPhase, setFirstPhase] = useState([]);
   const [nextPhase, setNextPhase] = useState(false);
   const [photo, setPhoto] = useState(null);
@@ -32,6 +34,44 @@ const Signup = () => {
     });
   };
 
+  const handlePassword = (value) => {
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+
+    if (!regex.test(value)) {
+      setPassValWarn({});
+
+      const validValues = [];
+      const lowercase = /(?=.*[a-z])/;
+      const uppercase = /(?=.*[A-Z])/;
+      const number = /(?=.*\d)/;
+      const character = /(?=.*\W)/;
+
+      if (lowercase.test(value))
+        !validValues.includes(lowercase) ? validValues.push("lowercase") : null;
+      if (uppercase.test(value))
+        !validValues.includes(uppercase) ? validValues.push("uppercase") : null;
+      if (number.test(value))
+        !validValues.includes(number) ? validValues.push("number") : null;
+      if (character.test(value))
+        !validValues.includes(character) ? validValues.push("character") : null;
+
+      validValues.map((elem) =>
+        setPassValWarn((prev) => {
+          return { ...prev, [elem]: true };
+        })
+      );
+
+      return false;
+    }
+
+    setPassValWarn({
+      lowercase: true,
+      uppercase: true,
+      number: true,
+      character: true,
+    });
+  };
+
   const ikSuccess = (response) => setPhoto(response);
 
   const handleNextPhase = (_) => {
@@ -41,8 +81,11 @@ const Signup = () => {
     } else if (passwordRef.current.value !== cfPasswordRef.current.value) {
       toast.error("Password and Confirm Password does not match!");
       return false;
-    } else if (passwordRef.current.value.length < 8) {
-      toast.error("Password should be at least 8 characters long!");
+    } else if (passwordRef.current.value.length < 6) {
+      toast.error("Password should be at least 6 characters long!");
+      return false;
+    } else if (Object.keys(passValWarn).length < 4) {
+      toast.error("Password expression does not match!");
       return false;
     }
 
@@ -108,12 +151,57 @@ const Signup = () => {
                 value={input.password}
                 className="peer input input-sm input-bordered text-[#e87425] w-full px-3 py-5 rounded focus:outline-none focus:ring-2 focus:ring-[#e87425] valid:ring-2 valid:ring-[#e87425]"
                 onChange={changeInput}
+                onKeyUp={(e) => handlePassword(e.target.value)}
+                onFocus={(_) => setPassVal(true)}
+                onBlur={(_) => setPassVal(false)}
                 required={true}
                 ref={passwordRef}
               />
               <label className="absolute top-0 left-0 ml-3 mt-2.5 text-gray-400 peer-focus:-translate-y-1/2 peer-focus:bg-[#e87425] peer-focus:text-white peer-focus:mt-0 peer-focus:px-2 peer-focus:rounded peer-valid:-translate-y-1/2 peer-valid:bg-[#e87425] peer-valid:text-white peer-valid:mt-0 peer-valid:px-2 peer-valid:rounded transition-all duration-300 pointer-events-none">
                 Password
               </label>
+            </div>
+            <div
+              className={`${
+                !passVal
+                  ? "max-h-0 opacity-0 !-mt-0"
+                  : "max-h-96 opacity-100 !mt-4"
+              } overflow-hidden transition-[max-height,_opacity, margin-top] duration-500`}
+            >
+              <div className="alert grid-flow-row grid-cols-1 rounded">
+                <div
+                  className={`flex items-center ${
+                    passValWarn.lowercase ? "text-green-500" : "text-red-500"
+                  } space-x-2`}
+                >
+                  <FaInfoCircle />
+                  <span>At least one lowercase</span>
+                </div>
+                <div
+                  className={`flex items-center ${
+                    passValWarn.uppercase ? "text-green-500" : "text-red-500"
+                  } space-x-2`}
+                >
+                  <FaInfoCircle />
+                  <span>At least one uppercase</span>
+                </div>
+                <div
+                  className={`flex items-center ${
+                    passValWarn.number ? "text-green-500" : "text-red-500"
+                  } space-x-2`}
+                >
+                  <FaInfoCircle />
+                  <span>At least one number</span>
+                </div>
+                <div
+                  className={`flex items-center ${
+                    passValWarn.character ? "text-green-500" : "text-red-500"
+                  } space-x-2`}
+                >
+                  <FaInfoCircle />
+                  <span>At least one character</span>
+                </div>
+              </div>
             </div>
             <div className="relative">
               <input
