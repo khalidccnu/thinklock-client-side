caac44c81a50e060f4a07b62075068915028d90d
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-08-03 20:56:42 +0600

    highlight added

---
+++ src/components/Highlight.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaEdit, FaUserPlus } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
+
+const Highlight = () => {
+  return (
+    <section className="pt-16">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 max-w-sm sm:max-w-none lg:max-w-4xl mx-auto">
+          <div className="bg-white px-12 py-8 border rounded-lg">
+            <div className="inline-flex justify-center items-center w-10 h-10 bg-green-600/20 text-green-700 rounded-full">
+              <FaUserPlus />
+            </div>
+            <h5 className="font-semibold mt-3">Expert Mentor</h5>
+          </div>
+          <div className="bg-white px-12 py-8 border rounded-lg">
+            <div className="inline-flex justify-center items-center w-10 h-10 bg-red-600/20 text-red-700 rounded-full">
+              <FaEdit />
+            </div>
+            <h5 className="font-semibold mt-3">Best In Class Content</h5>
+          </div>
+          <div className="bg-white px-12 py-8 border rounded-lg">
+            <div className="inline-flex justify-center items-center w-10 h-10 bg-yellow-600/20 text-yellow-700 rounded-full">
+              <FiSettings />
+            </div>
+            <h5 className="font-semibold mt-3">Growth Potential</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlight;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import HomeSlider from "../components/home/HomeSlider.jsx";
+import Highlight from "../components/Highlight.jsx";
 import PopularCourses from "../components/home/PopularCourses.jsx";
 import Instructors from "../components/home/Instructors.jsx";
 import Testimonial from "../components/home/Testimonial.jsx";
@@ -12,6 +13,7 @@ const Home = () => {
         <title>Home - ThinkLock</title>
       </Helmet>
       <HomeSlider />
+      <Highlight />
       <PopularCourses />
       <Instructors />
       <Testimonial />
